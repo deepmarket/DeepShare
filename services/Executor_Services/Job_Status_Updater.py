baba3b38370616f81f9180c54247964561e95165
susham kumar <susham.kumar@gmail.com>
2018-08-24 02:19:50 -0700

    Update Job_Status_Updater.py

--- services/Executor_Services/Job_Status_Updater.py
+++ services/Executor_Services/Job_Status_Updater.py
@@ -20,6 +20,65 @@ import json
 import subprocess
 
 
+
+# check job status
+def checkJobStatus(jobResults):
+    # check job status (Add corresponding code to fetch the job status)
+    jobStatus_url = requests.urlopen('http://131.252.209.102:8443/json/')
+    data = jobStatus_url.read()
+    encoding = jobStatus_url.info().get_content_charset('utf-8')
+    JSON_object = json.loads(data.decode(encoding))
+
+    # After submitting the Job through submitJob function, if the job status is checked before the job execution starts.
+    # checkJobStatus should be able to check the status of the job
+    # If there are no applications executed
+
+    if len(JSON_object['completedapps']) != 0:
+        for job in jobResults:
+            #we can use hashmap to improve the performance.
+            for app in JSON_object['completedapps']:
+                if job['_id'] == str(app['_id']):
+                    total_cores = app['cores']
+                    total_cost_memory = app['memoryperslave'] * prices[job['time_slot']].memory
+                    print(str(total_cost_memory) + " total memory cost")
+                    total_cost_cores = total_cores * prices[job['time_slot']].cpus
+                    totalcost=total_cost_cores+total_cost_memory
+                    print(str(total_cost_cores) + "total cores cost")
+                    print("Job:" + job['name'] + "completed in duration " + str(job['duration'] / 3600000.0) + "hrs")
+                    updateResult = jobs.update_one({"_id": ObjectId(str(job['_id']))}, {"$set": {"price": totalcost, "status": "COMPLETED", "updateOn": str(date.today())}})
+
+
+
+def update_job_status():
+    global todayDate,update_prices_required
+    #This block of code is executed when the script is run for the first time.
+    if (date.today() + timedelta(1) - todayDate).days == 1 and update_prices_required:
+        print("prices are downloaded into the array")
+        update_prices()
+
+    #This block of code is executed when the date has changed and update the prices with the recent ones.
+    if (date.today() + timedelta(1) - todayDate).days != 1:
+        #update both todayDate and prices
+        print("updating both the prices and today date")
+        update_prices()
+        todayDate=date.today()
+    checkDB()
+
+
+
+def update_prices():
+    global update_prices_required,prices
+    prices_url = requests.urlopen('http://131.252.209.102:8080/api/v1/pricing')
+    prices_data = prices_url.read()
+    encoding = prices_url.info().get_content_charset('utf-8')
+    prices_data_object = json.loads(prices_data.decode(encoding))
+    if len(prices_data_object['prices']) != 0:
+        for price in prices_data_object['prices']:
+            prices.append(price)
+    update_prices_required = False
+
+
+
 #Check DB for scheduled jobs
 def checkDB():
     print("Checking Database...")
@@ -38,45 +97,19 @@ def updateJobStatus(doc,curr_status):
         return updateResult
 #exit
 
-#check job status
-def checkJobStatus(jobResults):
-        #check job status (Add corresponding code to fetch the job status)
-        doc = jobResults[0]
-        jobStatus_url = requests.urlopen('http://131.252.209.102:8443/json/')
-        data = jobStatus_url.read()
-        encoding = jobStatus_url.info().get_content_charset('utf-8')
-        JSON_object = json.loads(data.decode(encoding))
-        
-        #After submitting the Job through submitJob function, if the job status is checked before the job execution starts.
-        #checkJobStatus should be able to check the status of the job
-        #If there are no applications executed
-        if len(JSON_object['activeapps']) != 0:   
-            for job in JSON_object['activeapps']:
-                if job['name'] == str(doc['_id']):
-                    print("App name " + job['name'] + ",doc id " + str(doc['_id']) +",startTime" + str(job['starttime']) + str(doc))
-                    #Update job status in database
-                    updateResult = updateJobStatus(doc,job['state'])
-                    #Reset the job status and job id
-                    JOB_STATUS = 0
-                    JOB_ID = 0
-        if len(JSON_object['completedapps']) != 0:
-             print("found completed apps")
-             for job in JSON_object['completedapps']:
-                print(job['name'], str(doc['_id']))
-                if job['name'] == str(doc['_id']):
-                    print("App name " + job['name'] + ",doc id " + str(doc['_id']) +",startTime" + str(job['starttime']))
-                    #Update job status in database
-                    updateResult = updateJobStatus(doc,job['state'])
 
-if __name__== "__main__":
-  #Database connection
-    client = MongoClient('localhost', 27017)
-    db=client.ShareResources
-    jobs = db.jobs
-    #Schedule to check the job status
-    schedule.every(30).seconds.do(lambda: checkDB())
+todayDate = date.today()
+update_prices_required = True
 
-    while True:
+prices = []
+client = MongoClient('localhost', 27017)
+db = client.ShareResources
+jobs = db.jobs
+#Schedule to check the job status
+schedule.every(5).seconds.do(lambda: update_job_status())
+
+
+while True:
         schedule.run_pending()
         time.sleep(1)
-    
+
